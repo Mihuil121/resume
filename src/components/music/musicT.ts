058683213243c0df67cd7719c8343166f3c6a18a
Mihuil121 <bezugliymicha@gmail.com>
2025-03-31 20:52:19 +0300

    update  musicT.ts.

--- src/components/music/musicT.ts
+++ src/components/music/musicT.ts
@@ -3,7 +3,10 @@ import { StaticImageData } from 'next/image'
 import audio from '../../audio/king.mp3'
 import audio2 from '../../audio/my.mp3'
 import photo2 from '../../image/music/photo_2025-02-23_12-41-19.jpg'
-
+import Twenty from '../../audio/Twenty-One-Pilots-Shy-Away-Livestream-Version.mp3'
+import photo3 from '../../image/music/twenty.jpg'
+import ajr from '../../audio/AJR-Yes-I-m-A-Mess-Official-Visualizer.mp3'
+import photo4 from '../../image/music/ajr.jpg'
 export interface Imusic {
     text: string,
     back: StaticImageData,
@@ -23,6 +26,14 @@ export const music: Imusic[] = [
         text:"Кошмары",
         back:photo2,
         audio:audio2
+    },{
+        text:"Shy-Away",
+        back:photo3,
+        audio:Twenty
+    },{
+        text:"Yes I`m A Mess",
+        back:photo4,
+        audio:ajr
     },
     
 ]
